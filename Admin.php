d5eb7e6b8b73042d5bd31c941e1dd64b10a8b960
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-04-13 18:30:17 -0400

    table_created

--- Admin.php
+++ Admin.php
@@ -12,30 +12,31 @@ include('./func/app.php')
                <form method="$_POST" action="" class="Add-product column">
                     
                       <div class="Add-product-div">
-                            <input type="text" name="Pname" placeholder="Enter product name">
+                            <input type="text" name="Pname" placeholder="Enter product name" required>
                       </div>
 
                       <div class="Add-product-div">
-                            <input type="number" name="Pprice" placeholder="Enter product price">
+                            <input type="number" name="Pprice" placeholder="Enter product price" required>
                       </div>
 
                       <div class="Add-product-div">
-                            <input type="text" name="Pdescrip" placeholder="Enter product description">
+                            <input type="text" name="Pdescrip" placeholder="Enter product description"required>
                       </div>
 
                       <div class="Add-product-div">
-                            <input type="file" name="Pimg" placeholder="Enter product">
-                      </div>
+                      <label for="customFile" class="imgLabel">Choose product Image</label> 
+                          <input type="file"  name="Pimg" id="customFile" placeholder="Enter product" required>
+                    </div>
 
                       <div class="Add-product-div">
-                            <select name="category">
+                            <select name="category" required>
                                      <option value="Controller">Controller</option>
                                      <option value="PcGaming">PcGaming</option>
                                      <option value="Console">Console</option>
                                      <option value="Accessory">Accessory</option>
                            </select>
                       </div>
-                      <input type="submit" value="Add">
+                      <input type="submit" name="submit" value="Add">
                </form>
       </section>
       <section>
