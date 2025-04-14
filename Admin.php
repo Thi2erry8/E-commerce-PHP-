8833031c4f2e6e9511bf341b124533958055a6db
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-04-13 20:08:43 -0400

    send to database

--- Admin.php
+++ Admin.php
@@ -3,13 +3,39 @@
 <head>
      <title>Admin Panel</title>
 <?php 
+$msg ="";
 include('./inc/header.php'); 
-include('./func/app.php')
+include('./func/app.php');
+   if(isset($_POST['submit'])){
+     
+      $p_name = $_POST['Pname'];
+      $p_price = $_POST['Pprice'];
+      $p_description = $_POST['Pdescrip'];
+
+      $target_dir = "image/";
+      $target_file = $target_dir.basename($_FILES['Pimg']['name']) ;
+      move_uploaded_file($_FILES['Pimg']['tmp_name'], $target_file);
+   
+      $p_category = $_POST['category'];
+      
+      $sql ="INSERT INTO 
+            product (product_name,product_price,product_description,product_img,product_category)
+             VALUES('$p_name','$p_price','$p_description','$target_file','$p_category') " ;
+
+      if(mysqli_query($conn,$sql)){
+            echo"ok";          /* $msg= "Product Added"; */
+            /* echo"<script>alert('Product Added')</script>"; */
+      }else{
+            echo" not ok";
+            /* $msg= "Product Not Added"; */
+            /* echo"<script>alert('Product Not Added')</script>"; */   
+      }
+}   
 ?>
 <main> 
       <section class=" Add-product-section column">
          <h1 >Add Product </h1>
-               <form method="$_POST" action="" class="Add-product column">
+               <form method="POST" action="" class="Add-product column" enctype="multipart/form-data">
                     
                       <div class="Add-product-div">
                             <input type="text" name="Pname" placeholder="Enter product name" required>
@@ -40,7 +66,7 @@ include('./func/app.php')
                </form>
       </section>
       <section>
-         <h1>All products</h1>
+         <h1>All products<?= $msg ;?></h1>
 
       </section>
 </main>
