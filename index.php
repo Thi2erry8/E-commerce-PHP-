87893bb37031c8a664074de02d01d8942ec84a33
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-05-16 22:19:18 -0400

    hero

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
      <title>Document</title>   
      <?php include('./inc/header.php') ?>
     <main>
-           <div style="background-image: url('./image/ps5_ontroller.jpg');" class="hero">
-                      <p>Le gaming est un art. Chez GamerHouse, nous fournissons les pinceaux.</p>
-</div>
+             <div style="background-image: url('./image/ps5_ontroller.jpg');" class="hero">
+                  <div class="hero-text">
+                        <p>Le gaming est un art. Chez GamerHouse, 
+                            nous fournissons les pinceaux.
+                        </p>
+                  </div>    
+             </div>
     </main>
        
