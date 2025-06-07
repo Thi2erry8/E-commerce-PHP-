866d6002b6c8d2819d9f3e5fea4210440b072d97
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-06-06 23:43:00 -0400

    0090

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php 
+    
+      include('./func/app.php');
+      //SelectDataProduct
      
+     
+      $sqlCont= "SELECT * FROM product WHERE product_category= 'Controller' " ;
+      $sqlCons= "SELECT * FROM product WHERE product_category= 'Console' " ;
+      $sqlPcgam= "SELECT * FROM product WHERE product_category= 'PcGaming' " ;
+      $sqlAcess= "SELECT * FROM product WHERE product_category= 'Accessory' " ;
+
+      //ResultForProduct
+      $resultCont = mysqli_query($conn,$sqlCont);
+      $resultCons = mysqli_query($conn,$sqlCons);
+      $resultPcgam = mysqli_query($conn,$sqlPcgam);
+      $resultAcess = mysqli_query($conn,$sqlAcess);
+
+      //
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,18 +36,26 @@
                   </div> 
              </div>
              <section class="section_product">
-                       <div class="row">
-                             <div class="product_card2">
+                       <div class=" container_product row">
+                        <div class="Category">
+                               <p>Controller</p>
+                        </div>
+                       <?php
+                            while($row=mysqli_fetch_array($resultCont)){
+                        ?>
+                                                                <div class="product_card2">
+
                                        <p style="display:none">27'</p>
                                        <div class="id_circle2">
                                            <i class="ri-heart-line"></i>
                                        </div>
                                        <div class="img_box">
-                                           <img class="box_img" src="image/b0b8978e0b3e3ed978322d426f796d70.jpg" alt="GAMDIAS ATX Mid Tower">
+                                           <img class="box_img" src="<?= $row['product_img'] ?>" alt="GAMDIAS ATX Mid Tower">
                                        </div>
                                        <div class="info_box colunm">
-                                           <p class="p_one"> GAMDIAS ATX Mid Tower</p> 
-                                           <p>Black</p> 
+                                             <p class="p_one"> <?= $row['product_name'] ?></p> 
+                                             <p><?= $row['color'] ?></p> 
+                                             <p><?= $row['product_price'] ?> $</p>
                                       </div>
                                       <div class="action_box row">
                                            <button class="action_box_btn2">
@@ -46,6 +71,10 @@
                                            </button>     
                                       </div>
                                    </div>
+                        <?php
+                            }
+                        ?>  
+
                        </div>
              </section>
     </main>
