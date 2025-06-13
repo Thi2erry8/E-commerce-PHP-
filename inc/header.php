c51fd56ef6f9f30c7030c0547302d708fe56db82
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-06-13 00:29:52 -0400

    prfilre_frontend_begin

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./asset/style/style.css">
+    <link rel="stylesheet" href="./asset/style/style2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- profil.php
+++ profil.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+     include('./inc/header.php'); 
+     include('./func/app.php');
+?>
+<main>
+       <section class="product_info">
+                 <!-- <div class="row"> -->
+                       <div class="product_img_container">
+                             <img class="product_img" src="./image/ps5_ontroller.jpg" alt="">
+                       </div>
+                       <div class="column product_description">
+                             <p>Ps5 controller</p>
+                             <p>controller</p>
+                             <p>150$</p>
+                             <p>white</p>
+                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nesciunt eum dolorum dignissimos nisi? At laudantium repellendus sapiente laboriosam exercitationem.</p>
+                             <button> Add to card </button>
+                       </div>
+                 <!-- </div> -->
+       </section>
+</main>
+<?php include('./inc/footer.php') ?>
