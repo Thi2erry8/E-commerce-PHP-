32a2ceb7bb0af11e9c1802ab03d6a5a4798f595d
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-08-03 21:49:56 -0400

    styling_2

--- index.php
+++ index.php
@@ -94,28 +94,29 @@
                      
                       
                        <!-- Controller -->
-                     <div class=" container_product row">
-                        <div class="Category">
-                               <h2>Controller</h2>
-                        </div>
-                       <?php
-                            while($row=mysqli_fetch_array($resultCont)){
-                        ?>
-                                                                <div class="product_card2">
-
-                                       <p style="display:none">27'</p>
-                                       <div class="id_circle2">
+                     <div class="column">
+                                  <div class="Category">
+                                       <h2>Controller</h2>
+                                  </div>
+             
+                                  <div class=" container_product row">
+                                   <?php
+                                       while($row=mysqli_fetch_array($resultCont)){
+                                     ?>
+                                        <div class="product_card2">
+                                            <p style="display:none">27'</p>
+                                         <div class="id_circle2">
                                            <i class="ri-heart-line"></i>
-                                       </div>
-                                       <div class="img_box">
+                                         </div>
+                                         <div class="img_box">
                                            <img class="box_img" src="<?= $row['product_img'] ?>" alt="GAMDIAS ATX Mid Tower">
-                                       </div>
-                                       <div class="info_box colunm">
-                                             <p class="p_one"> <?= $row['product_name'] ?></p> 
-                                             <p><?= $row['color'] ?></p> 
-                                             <p><?= $row['product_price'] ?> $</p>
-                                      </div>
-                                      <div class="action_box row">
+                                         </div>
+                                         <div class="info_box colunm">
+                                                <p class="p_one"> <?= $row['product_name'] ?></p> 
+                                                <p><?= $row['color'] ?></p> 
+                                                <p><?= $row['product_price'] ?> $</p>
+                                        </div>
+                                        <div class="action_box row">
                                            <button class="action_box_btn2">
                                                  <a href='./profil.php?id= <?=$row['id'] ?>'>
                                                     <i class="ri-information-line"></i>
@@ -129,11 +130,13 @@
                                            </button>     
                                       </div>
                                    </div>
-                        <?php
-                            }
-                        ?>  
+                                     <?php
+                                        }
+                                     ?>  
 
-                       </div>
+                              </div>
+                     </div>
+                     
                        
                                               <div class=" container_product row">
                         <div class="Category">
