c013268d9de7051bd1725bfadbd62ad1696c144c
Thi2erry8 <123498029+Thi2erry8@users.noreply.github.com>
2025-09-08 11:48:39 -0400

    Update connect.php

--- func/connect.php
+++ func/connect.php
@@ -1,9 +1,9 @@
 <?php 
 
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "gamerhouse";
+$db_server = "sql105.infinityfree.com";
+$db_user = "if0_39849868";
+$db_pass = "nograS009k5MvfO";
+$db_name = "f0_39849868_portfolio";
 
 
 $conn = mysqli_connect($db_server,
