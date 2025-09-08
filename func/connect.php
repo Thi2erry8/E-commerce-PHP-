4c2581c9c719c4be7fbc454ad83cf5e6f2afd49d
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-08 19:48:19 -0400

    test3

--- func/connect.php
+++ func/connect.php
@@ -1,11 +1,12 @@
 <?php 
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 echo "✅ PHP fonctionne<br>";
 $db_server = "sql105.infinityfree.com";
 $db_user = "if0_39849868";
 $db_pass = "nograS009k5MvfO";
-$db_name = "f0_39849868_portfolio";
+$db_name = "if0_39849868_portfolio";
 
 
 $conn = mysqli_connect($db_server,
