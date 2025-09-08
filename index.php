7266742bd0b70222fec3bbff8e778562b90d9dee
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-08 19:06:34 -0400

    01010101

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 
     include('./func/app.php');
       //SelectDataProduct
-     ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php-error.log');
+
+
      
       $sqlCont= "SELECT * FROM product WHERE product_category= 'Controller' " ;
       $sqlCons= "SELECT * FROM product WHERE product_category= 'Console' " ;
